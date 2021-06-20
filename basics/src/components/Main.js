b1c626235a7c16e996cc2c2aab018bda74e5aea1
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-06-21 00:21:50 +0530

    final

--- basics/src/components/Main.js
+++ basics/src/components/Main.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "../redux/actions/main";
 
 const Main = () => {
+  const initValue = useSelector((state) => state.main);
+
+  const dispatch = useDispatch();
+
+  const inc = () => {
+    dispatch(incNumber());
+  };
+
+  const dec = () => {
+    dispatch(decNumber());
+  };
+
   return (
     <div className="main">
       <div className="main-head">Simple Redux Basics</div>
       <div className="main-body">
-        <button className="btn">-</button>
-        <input type="text" value="0" className="main-text-input" />
-        <button className="btn">+</button>
+        <button onClick={dec} className="btn">
+          -
+        </button>
+        <input
+          type="text"
+          readOnly
+          value={initValue}
+          className="main-text-input"
+        />
+        <button onClick={inc} className="btn">
+          +
+        </button>
       </div>
     </div>
   );
