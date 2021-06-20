36e308b90adf0be9f09e5072e8037f0f3aecbb54
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-06-20 19:52:01 +0530

    layout

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Main from "./components/Main";
 
 const App = () => {
-  return <div>redux2</div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 };
 
 export default App;
---
+++ basics/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <div className="main-head">Simple Redux Basics</div>
+      <div className="main-body">
+        <button className="btn">-</button>
+        <input type="text" value="0" />
+        <button className="btn">+</button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
