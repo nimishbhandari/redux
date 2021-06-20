5bf3efceb0f1203f41d1a8923b7201da37d03ecc
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-06-21 00:40:51 +0530

    basic redux setup

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "./components/Main";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <Main />
-    </div>
+    </Provider>
   );
 };
 
